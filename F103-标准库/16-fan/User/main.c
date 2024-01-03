0e47d1b2c4c9e16dd0d3678fa343bf6bf20532b7
Zi_Chuan <d3326145@163.com>
2024-01-03 21:42:04 +0800

    已完成

--- F103-标准库/16-fan/User/main.c
+++ F103-标准库/16-fan/User/main.c
@@ -10,23 +10,35 @@
 
 #include "stm32f10x.h"
 #include "delay.h"
+#include "HC05.h"
 #include "Fan.h"
+#include "bsp_usart.h"
 
 
+uint8_t RxSTA = 1;
+int RxData[100];
 
 int main(void)
 	
 {
-	
+	Usart1_Config();
+	BlueTooth_Init();
 	Fan_Init();
 
     while (1)
     {
-		Fan_ON();
-		Delay_s(4);
-		Fan_OFF();
-		Delay_s(3);
-
+		HC05_GetData(RxData);
+		if(RxSTA == 0)
+		{
+			if(RxData[0] == '1')
+			{
+				Fan_ON();
+			}
+			if(RxData[0] == '0')
+			{
+				Fan_OFF();
+			}
+		}
     }
 }
 
