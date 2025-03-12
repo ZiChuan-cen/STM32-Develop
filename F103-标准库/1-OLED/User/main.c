3b3b01e217a45b8ffee2f3c50e943fd299f7c045
Xzc <d3326145@163.com>
2025-03-12 20:59:57 +0800

    清除缓存文件

--- F103-标准库/1-OLED/User/main.c
+++ F103-标准库/1-OLED/User/main.c
@@ -16,29 +16,29 @@
 
 int main(void)
 {
-    OLED_Init();
+  OLED_Init();
 
-    OLED_ShowChar(1, 1, 'A');
-    OLED_ShowChar(2, 2, 'B');
-    OLED_ShowChar(3, 3, 'C');
-    OLED_ShowChar(4, 4, 'D');
-	OLED_ShowString(1, 4, "zichuan");
-	OLED_ShowNum(2, 4, 1121, 4);
-	OLED_ShowHexNum(3, 6, 0x1122, 4);
-	OLED_ShowSignedNum(4, 8, -99, 2);
-	Delay_ms(4000);
-	
-    OLED_ShowChinese(1, 1, 0);
-    OLED_ShowChinese(1, 2, 1);
-    OLED_ShowChinese(1, 3, 2);
-    OLED_ShowChinese(1, 4, 3);
-    OLED_ShowChinese(1, 5, 4);
-    OLED_ShowChinese(1, 6, 5);
+  OLED_ShowChar(1, 1, 'A');
+  OLED_ShowChar(2, 2, 'B');
+  OLED_ShowChar(3, 3, 'C');
+  OLED_ShowChar(4, 4, 'D');
+  OLED_ShowString(1, 4, "zichuan");
+  OLED_ShowNum(2, 4, 1121, 4);
+  OLED_ShowHexNum(3, 6, 0x1122, 4);
+  OLED_ShowSignedNum(4, 8, -99, 2);
+  Delay_ms(4000);
+
+  OLED_ShowChinese(1, 1, 0);
+  OLED_ShowChinese(1, 2, 1);
+  OLED_ShowChinese(1, 3, 2);
+  OLED_ShowChinese(1, 4, 3);
+  OLED_ShowChinese(1, 5, 4);
+  OLED_ShowChinese(1, 6, 5);
 //	OLED_BMP(3);
 //	Delay_ms(2000);
-    //OLED_Clear();
+  //OLED_Clear();
 
-    while (1)
+  while (1)
     {
 //		for(int i=3; i<9; i++)
 //		{
